304a690d0856cc5bd13cc333506ffecdf96325a0
Ryan <rizzo@unca.edu>
2025-02-24 15:14:18 -0500

    Tutorial 5 Build 1.0

--- tutorials/tutorial05/03-carousel/index.js
+++ tutorials/tutorial05/03-carousel/index.js
@@ -1,10 +1,13 @@
-let currentPosition = 0;
-let gap = 10;
-const slideWidth = 400;
+let currentPosition = 0; //Keeps track of carousel current position
+let gap = 10; //defines the gap between each slide in the carousel
+const slideWidth = 400; //defines width of every slide (fixed)
 
+//Function to move the carousel backwards & forwards
 function moveCarousel(direction) {
-    const items = document.querySelectorAll(".carousel-item");
+    const items = document.querySelectorAll(".carousel-item"); //selects all HTML in the "carousel-item" class and returns NodeList
 
+    //checking to see if the direction is forward or backward
+    //forward
     if (direction == "forward") {
         // minus 2 b/c first 2 slides already showing
         if (currentPosition >= items.length - 2) {
@@ -12,14 +15,16 @@ function moveCarousel(direction) {
         }
         currentPosition++;
     } else {
+        //backwards
         if (currentPosition == 0) {
             return false;
         }
         currentPosition--;
     }
 
-    const offset = (slideWidth + gap) * currentPosition;
+    const offset = (slideWidth + gap) * currentPosition; // determines how much the slides need to move horizontally
 
+    //goes through all carousel items
     for (const item of items) {
         item.style.transform = `translateX(-${offset}px)`;
     }
