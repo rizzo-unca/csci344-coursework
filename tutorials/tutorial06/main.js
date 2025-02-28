05d594d2c8b1ccd7f694226f4a1429c01177602e
Ryan <rizzo@unca.edu>
2025-02-28 13:16:01 -0500

    Tutorial 6 Build 0.0

--- tutorials/tutorial06/main.js
+++ tutorials/tutorial06/main.js
@@ -8,21 +8,41 @@ const search = (ev) => {
     // Set user's preferences (global variables) from the DOM:
     searchTerm = document.querySelector("#search_term").value;
     openOnly = document.querySelector("#is_open").checked;
+    console.log(searchTerm, openOnly);
 
     // Invoke the show matching courses function
     showMatchingCourses();
 };
 
+/*************************/
+/* First Function I made */
+/*************************/
 // Part 1.1a
 const isClassFull = (course) => {
-    // modify this to accurately apply the filter:
-    return true;
+    //look at classification open: true
+    return course.EnrollmentCurrent >= course.EnrollmentMax;
+    /*
+    if (course.EnrollmentCurrent >= course.EnrollmentMax) {
+        return true;
+    } else {
+        return false;
+    }
+    */
 };
 
+
+/**************************/
+/* Second Function I made */
+/**************************/
 // Part 1.1b
-const doesTermMatch = (course) => {
-    // modify this to accurately apply the filter:
-    return true;
+const doesTermMatch = (courseList) => {
+    let match = false;
+    //title check
+    if (course.Title.toLowerCase().includes(searchTerm.toLowerCase())) {
+        match = true;
+    }
+
+    //could check multiple things (title matching)
 };
 
 // Part 1.2
