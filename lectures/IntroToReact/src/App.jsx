9dd16f63fc6800f0e18d0eb1b4a1c5960be2b4fb
Ryan <rizzo@unca.edu>
2025-03-21 13:32:41 -0400

    React Test

---
+++ lectures/IntroToReact/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+ export default function App() {
+
+     return (
+         <>
+             <header>
+                 <h1>My First App</h1>
+             </header>
+             <main>
+                 <p>Hello React!</p>
+             </main>
+         </>
+     );
+ }
